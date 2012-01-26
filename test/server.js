22781811abe8d9a430f5a74547d215d55a549ce4
Jake Luer <jake.luer@gmail.com>
2012-01-26 03:46:05 -0500

    server listening / closing + tests

--- test/server.js
+++ test/server.js
@@ -21,4 +21,27 @@ describe('server', function () {
     serv.emit('hello::universe', 1);
     serv.emit('hello::world', 2);
   });
+
+  describe('listinging / closing', function () {
+    var serv = new orchid.Server();
+
+    it('should listen on a port', function (done) {
+      serv.listen(8080, done);
+    });
+
+    it('should close and trigger callback', function (done) {
+      serv.close(done);
+    });
+
+    it('should emit listening event', function (done) {
+      serv.once('listening', done);
+      serv.listen(8080);
+    });
+
+    it('should emit close event', function (done) {
+      serv.once('close', done);
+      serv.close();
+    });
+  });
+
 });
